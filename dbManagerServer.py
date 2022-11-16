dd0f6fcf11a6a38861e88b6a7bfe3e4ba451c549
Noa-k-git <104233360+Noa-k-git@users.noreply.github.com>
2022-11-16 12:24:42 +0200

    a bit documentation

--- dbManagerServer.py
+++ dbManagerServer.py
@@ -25,21 +25,33 @@ class dbManagerServer(Server):
         self.db.union()
         
     def handle_client(self, current_socket:socket.socket, data:str):
+        """All client's requests end up here. 
+        The function append the request to the queue for db handling,
+        and call the queue manager if it is the first request.
+
+        Args:
+            current_socket (socket.socket): the client
+            data (str): client request
+        """
         request = data.split(':', 2)
-        if len(request) == 2:
+        if len(request) == 2: # checks for index error
             request = request[0], request[1].split(',', 2)
             self.queue.append((current_socket, request))
 
         if len(self.queue) == 1:
-            manage_queue_thread = threading.Thread(target=self.manage_queue, args=(current_socket,))
+            manage_queue_thread = threading.Thread(target=self.manage_queue)
             manage_queue_thread.start()
 
-    def manage_queue(self, current_socket:socket.socket):
-        self.queue_lock.acquire()
-        while not len(self.queue) == 0:
+    def manage_queue(self):
+        """This function manages the request's queue.
+        """
+        self.queue_lock.acquire() # making sure only one thread is using this function
+        while not len(self.queue) == 0: # while there are requests in the queue
+            # getting the data for the first element
             sock = self.queue[0][0]
             cmd = self.queue[0][1][0]
             args = self.queue[0][1][1]
+            # if db properties are available
             if not self.all_locked[1]:
                 if cmd.lower() == 'read':
                     if (self.current_writing[1] == None or not args[0] == self.current_writing[1][1][0]) and len(self.current_reading) <= 10:
@@ -50,7 +62,7 @@ class dbManagerServer(Server):
                         del self.queue[0]
                 
                 elif cmd.lower() == 'update':
-                    self.update_lock.acquire()
+                    # self.update_lock.acquire()
                     if self.current_writing[1] == None:
                         self.current_writing = [False, (sock, args)]
                         del self.queue[0]
@@ -58,7 +70,7 @@ class dbManagerServer(Server):
                             self.current_writing[0] = True
                             thread = threading.Thread(target=self.write)
                             thread.start()
-                    self.update_lock.release()
+                    # self.update_lock.release()
                 
                 elif cmd.lower() == 'admin_lock_0000':
                     self.all_locked[0] = sock
@@ -68,7 +80,7 @@ class dbManagerServer(Server):
 
                     
             elif cmd.lower() == 'release_r':
-                self.release_reader(current_socket)
+                self.release_reader(sock)
                 del self.queue[0]
                         
             elif cmd.lower() == 'release_w':
@@ -86,17 +98,20 @@ class dbManagerServer(Server):
                         self.union_ok_message = True
                     else:
                         time.sleep(0.05)
-                    
+            
+            # checks if the changes file larger than 7kb
             if os.path.getsize(self.db.changes_path) > 7000 and not self.union_started:
                 self.union_started = True
                 # client_process = multiprocessing.Process(target=self.union_client)
                 # client_process.start()
-                client_thread = threading.Thread(target=self.union_client)
+                client_thread = threading.Thread(target=self.merge_client)
                 client_thread.start()
 
         self.queue_lock.release()
         
-    def union_client(self):
+    def merge_client(self):
+        """Merge the two data bases
+        """
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         my_socket.connect((self.IP, self.PORT))
         my_socket.send(b'admin_lock_0000:')
